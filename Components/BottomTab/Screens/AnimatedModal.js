a3f132866356035c54c002700c940757f41904a5
YasirAliSmit <yasir.ali75404@gmail.com>
2023-07-03 06:32:04 +0500

    done modal work

--- Components/BottomTab/Screens/AnimatedModal.js
+++ Components/BottomTab/Screens/AnimatedModal.js
@@ -11,89 +11,161 @@ import {
   responsiveScreenHeight,
   responsiveScreenWidth,
 } from 'react-native-responsive-dimensions';
-import ConfettiCannon from 'react-native-confetti-cannon'
+import ConfettiCannon from 'react-native-confetti-cannon';
 import Entypo from 'react-native-vector-icons/dist/Entypo';
 import {useEffect} from 'react';
-import { transform } from 'lodash';
+import {transform} from 'lodash';
 
 const AnimatedModal = ({closeModal}) => {
-  const [showImage,setShowImage] = useState(true)
-  const [startAmination,setStartAnimation] =useState(false)
+  const [showImage, setShowImage] = useState(true);
+  const [startAmination, setStartAnimation] = useState(false);
+  const [showDesImage,setShowDesImage] = useState(false)
   const animation = useRef(new Animated.Value(0)).current; /// this is reference of animation in side animation.value we need to pass intialValue
   const animationValue = useRef(new Animated.Value(0)).current;
   const isFlipped = useRef(false);
-  const animatedValue = new Animated.Value(0)
-  let currentValue = 0 
- const images = [
-{
-  id:1,
-  front:'../../assets/1x/front.png',
-  back:'../../assets/1x/back.png'
-},{
-  id:2,
-  front:'../../assets/1x/front.png',
-  back:'../../assets/1x/back.png'
-},{
-  id:3,
-  front:'../../assets/1x/front.png',
-  back:'../../assets/1x/back.png'
-},
- ]
-  animatedValue.addListener(({value})=>{
-    currentValue = value 
-  })
+  const animatedValue = new Animated.Value(0);
+
+  const [isVisible, setIsVisible] = useState(false);
+  const opacityAnimation = useRef(new Animated.Value(0)).current;
+  let currentValue = 0;
+  const animationCounter = useRef(0);
+  const images = [
+    {
+      id: 1,
+      front: '../../assets/1x/front.png',
+      back: '../../assets/1x/back.png',
+    },
+    {
+      id: 2,
+      front: '../../assets/1x/front.png',
+      back: '../../assets/1x/back.png',
+    },
+    {
+      id: 3,
+      front: '../../assets/1x/front.png',
+      back: '../../assets/1x/back.png',
+    },
+  ];
+  animatedValue.addListener(({value}) => {
+    currentValue = value;
+  });
   const setInterpolate = animatedValue.interpolate({
-    inputRange:[0,180],
-    outputRange:['180deg','360deg']
- 
-  })
+    inputRange: [0, 180],
+    outputRange: ['180deg', '360deg'],
+  });
   const rotateYAnimatedStyle = {
-    transform:[{rotateY:setInterpolate}]
-  }
-  
+    transform: [{rotateY: setInterpolate}],
+  };
+
+  const showHideImage = () => {
+    Animated.sequence([
+      Animated.timing(opacityAnimation, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.delay(500),
+      Animated.timing(opacityAnimation, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.delay(500),
+    ]).start(() => {
+      animationCounter.current++;
+      if (animationCounter.current < 3) {
+        showHideImage();
+      } else {
+        setIsVisible(false);
+        animationCounter.current = 0; // Reset counter for future animations
+      }
+    });
+  };
+  const startAnimation = () => {
+    setIsVisible(true);
+
+    const showHideImage = () => {
+      Animated.sequence([
+        Animated.timing(opacityAnimation, {
+          toValue: 1,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+        Animated.delay(500),
+        Animated.timing(opacityAnimation, {
+          toValue: 0,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+        Animated.delay(500),
+      ]).start(() => {
+        animationCounter.current++;
+        if (animationCounter.current < 3) {
+          showHideImage();
+        } else {
+          setIsVisible(false);
+          animationCounter.current = 0; // Reset counter for future animations
+        }
+      });
+    };
+
+    showHideImage();
+  };
   const startAnimations = () => {
     Animated.spring(animation, {
       toValue: 3000,
       useNativeDriver: true,
     }).start(); // its is animation spring or start  spring take two parameter first reference and object in side object u can pass from and two like toValue
   };
-const flipAnimation = () => {
-  if(currentValue >= 90){
-    Animated.spring(animatedValue,{
-      toValue:0,
-      tension:10,
-      // friction:8,
-      useNativeDriver:true
-    }).start()
-  }else{
-    Animated.spring(animatedValue,{
-      toValue:180,
-      
-       tension:10,
-      // friction:8,
-      useNativeDriver:false
-    }).start()
-  }
-  setInterval(()=>{setShowImage(false)},50)
-}
+  const flipAnimation = () => {
+    if (currentValue >= 90) {
+      Animated.spring(animatedValue, {
+        toValue: 0,
+        tension: 10,
+        // friction:8,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.spring(animatedValue, {
+        toValue: 180,
 
-const startCardAnition = ()=>{
-  // Animated.spring(animation,{
-  //   toValue:1
-  //   ,useNativeDriver:true
-  // }).start()
-  Animated.timing(animation, {
-    toValue: 1,
-    duration: 1500,
-    useNativeDriver: true,
-  }).start();
-}
+        tension: 10,
+
+        useNativeDriver: false,
+      }).start();
+    }
+    setTimeout(() => {
+      setShowImage(false);
+    }, 50);
+
+    setTimeout(()=>{    setShowImage(true);},3000)
+    setTimeout(()=>{    setShowDesImage(true);},4000)
+  };
+
+  const startCardAnition = () => {
+    Animated.sequence([
+      Animated.timing(animation, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 1500,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+  function toggleFunction(){
+    console.log('Hello form  togglefunction ')
+  }
   return (
     <View style={styles.modalContainer}>
       <Image
         style={styles.screenImage}
         source={require('../../assets/1x/screen.png')}
       />
+    
       <Animated.Image
         style={[
           styles.lines,
@@ -112,62 +184,104 @@ const startCardAnition = ()=>{
       />
       <View style={{flexDirection: 'row'}}>
         <TouchableOpacity onPress={() => console.log('Hello')}>
-        <View>
-            <Image 
-              style={[styles.element]}
-              source={require('../../assets/1x/element.png')}
-            />
-             <Image
+          <View>
+            {isVisible && (
+              <Animated.Image
+                style={[styles.element, {opacity: opacityAnimation}]}
+                source={require('../../assets/1x/element.png')}
+              />
+            )}
+            <Image
               style={styles.heading}
               source={require('../../assets/1x/heading.png')}
             />
           </View>
-            </TouchableOpacity>
-          <View>
-            <TouchableOpacity onPress={() => console.log('tttttttttt')}>
+        </TouchableOpacity>
+        <View>
+          <TouchableOpacity>
             <Animated.Image
-             // style={[styles.front,rotateYAnimatedStyle]}
-             style={[styles.front,rotateYAnimatedStyle,{transform:[{translateX:animation.interpolate({
-              inputRange:[0,1],
-              outputRange:[0,120]
-            })}]}]}
-
-             //source={require('../../assets/1x/front.png')}
-              source={showImage?require('../../assets/1x/front.png'):require('../../assets/1x/back.png')}
-            /></TouchableOpacity>
-          </View>
-          <Animated.Image
-              style={[styles.front1,rotateYAnimatedStyle,{transform:[{translateX:animation.interpolate({
-                inputRange:[0,1],
-                outputRange:[0,120]
-              })}]}]}
-              // style={[styles.front1,rotateYAnimatedStyle]}
-              //source={require('../../assets/1x/front.png')}
-              source={showImage?require('../../assets/1x/front.png'):require('../../assets/1x/back.png')}
+              onPress={()=>console.log('New')}
+              style={[
+                styles.front,
+                rotateYAnimatedStyle,
+                {
+                  transform: [
+                    {
+                      translateX: animation.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [0, 120],
+                      }),
+                    },
+                  ],
+                },
+              ]}
+              source={
+                showImage
+                ? require('../../assets/1x/front.png')
+                : require('../../assets/1x/back.png')
+              }
             />
-       <Animated.Image
-              //style={[styles.front2,rotateYAnimatedStyle]}
-              //source={require('../../assets/1x/front.png')}
-              style={[styles.front2,rotateYAnimatedStyle,{transform:[{translateX:animation.interpolate({
-                inputRange:[0,1],
-                outputRange:[0,-230]
-              })}]}]}
+          </TouchableOpacity>
+        </View>
+                    <TouchableOpacity onPress={() => toggleFunction()}>
 
-              source={showImage?require('../../assets/1x/front.png'):require('../../assets/1x/back.png')}
-            />
-
+         <Animated.Image
+            style={[
+              styles.front1,
+              rotateYAnimatedStyle,
+              {
+                transform: [
+                  {
+                    translateX: animation.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, 120],
+                    }),
+                  },
+                ],
+              },
+            ]}
+            source={
+              showImage
+              ? require('../../assets/1x/front.png')
+              : require('../../assets/1x/back.png')
+            }
+          />
+        </TouchableOpacity>
+        <Animated.Image
+          style={[
+            styles.front2,
+            rotateYAnimatedStyle,
+            {
+              transform: [
+                {
+                  translateX: animation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, -230],
+                  }),
+                },
+              ],
+            },
+          ]}
+          source={
+            showImage
+            ? require('../../assets/1x/front.png')
+            : require('../../assets/1x/back.png')
+          }
+        />
       </View>
-      <TouchableOpacity onPress={() =>{ 
-        
-        setStartAnimation(true)
-        startAnimations()
-      flipAnimation() 
-      startCardAnition()
-      }}>
+      <TouchableOpacity
+        onPress={() => {
+          setStartAnimation(true);
+          startAnimations();
+          flipAnimation();
+          startCardAnition();
+          startAnimation();
+        }}>
         <Image
           style={styles.button}
           source={require('../../assets/1x/button.png')}
-        />
+          />
+          {showDesImage?(<><Image style={styles.imagebackSprize} source={ require('../../assets/1x/front.png')}/><ConfettiCannon style={{zIndex:20}} count={150} origin={{x: 10, y: 10}} /></>):(null)} 
       </TouchableOpacity>
       <Entypo
         style={styles.cross}
@@ -180,16 +294,24 @@ const startCardAnition = ()=>{
   );
 };
 
-export default AnimatedModal;
+export default React.memo(AnimatedModal);
 
 const styles = StyleSheet.create({
- /// modalContainer: { height: responsiveScreenHeight(40),},
   screenImage: {
     borderRadius: responsiveScreenWidth(8),
     height: responsiveScreenHeight(30),
     width: responsiveScreenWidth(90),
     resizeMode: 'contain',
     position: 'relative',
+  },
+  imagebackSprize:{
+    position:"absolute",
+    width:responsiveScreenWidth(50),
+   // height:responsiveScreenHeight(50),
+    resizeMode:'contain',
+    top:responsiveScreenHeight(-30),
+    left:responsiveScreenWidth(20),
+    zIndex:10
   },
   lines: {
     position: 'absolute',
@@ -200,7 +322,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     height: responsiveScreenHeight(20),
     width: responsiveScreenWidth(20),
-    zIndex: 20,
+ 
     resizeMode: 'contain',
     top: responsiveScreenHeight(-13),
     left: responsiveScreenWidth(35),
@@ -235,24 +357,25 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     top: responsiveScreenHeight(-23),
     left: responsiveScreenWidth(60),
-  },element:{
+  },
+  element: {
     position: 'absolute',
     top: responsiveScreenHeight(-30),
     //right: responsiveScreenWidth(6),
-    //zIndex:20,
+
     width: responsiveScreenWidth(70),
     //alignItems:"center",
     resizeMode: 'contain',
-    marginLeft:responsiveScreenWidth(10)
-  },heading:{
-    
+    marginLeft: responsiveScreenWidth(10),
+  },
+  heading: {
     position: 'absolute',
     top: responsiveScreenHeight(-35),
     left: responsiveScreenWidth(9),
-    //zIndex:20,
+
     width: responsiveScreenWidth(50),
     //alignItems:"center",
     resizeMode: 'contain',
-    marginLeft:responsiveScreenWidth(10)
-  }
+    marginLeft: responsiveScreenWidth(10),
+  },
 });
